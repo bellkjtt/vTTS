bd9f44bd20d038369e9a7108c6b8e3464f298486
bellkjtt <bellkjtt@gmail.com>
2026-01-23 13:50:09 +0900

    fix: matcha utils 의존성 최소화 (hydra, lightning 제거)

--- vtts/engines/_cosyvoice/matcha/utils/__init__.py
+++ vtts/engines/_cosyvoice/matcha/utils/__init__.py
@@ -1,5 +1,2 @@
-from vtts.engines._cosyvoice.matcha.utils.instantiators import instantiate_callbacks, instantiate_loggers
-from vtts.engines._cosyvoice.matcha.utils.logging_utils import log_hyperparameters
+"""Matcha-TTS utils - minimal version for CosyVoice"""
 from vtts.engines._cosyvoice.matcha.utils.pylogger import get_pylogger
-from vtts.engines._cosyvoice.matcha.utils.rich_utils import enforce_tags, print_config_tree
-from vtts.engines._cosyvoice.matcha.utils.utils import extras, get_metric_value, task_wrapper
